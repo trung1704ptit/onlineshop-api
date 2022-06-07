490a71d9a4d3be7f4525f13e35ef6b3e47ce92a1
Trung Nguyen <trung1704ptit@gmail.com>
2022-06-07 11:19:32 +0700

    fix duplicate ancestors

--- src/models/category.model.js
+++ src/models/category.model.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
-const { toJSON } = require('./plugins');
+// const { toJSON } = require('./plugins');
 const slugify = require('../utils/slugify');
 
 const categorySchema = mongoose.Schema({
   name: String,
   slug: {
     type: String,
-    unique: true
+    unique: true,
   },
   icon: {
     type: String,
@@ -14,36 +14,36 @@ const categorySchema = mongoose.Schema({
   },
   thumbnail: {
     type: String,
-    default: null
+    default: null,
   },
   parent: {
     type: mongoose.Schema.Types.ObjectId,
     default: null,
-    ref: 'Category'
+    ref: 'Category',
   },
   isShow: {
     type: Boolean,
-    default: true
+    default: true,
   },
-  ancestors: [{
-    _id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Category',
-      unique: true
-    }
-  }]
+  ancestors: [
+    {
+      _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+      },
+    },
+  ],
 });
 
 categorySchema.pre('save', async function (next) {
   this.slug = slugify(this.name);
   next();
-})
+});
 
 // add plugin that converts mongoose to json
 // categorySchema.plugin(toJSON);
 
 /* @typedef Category */
 const Category = mongoose.model('Category', categorySchema);
-
 
 module.exports = Category;
